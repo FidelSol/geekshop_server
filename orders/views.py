69d190e7b3095bbfe0a85e72a6d5ed9b520052df
FidelSol <fsoloviov2020@mail.ru>
2021-09-26 23:24:36 +0300

    Урок 4 Django Framework. Инструменты оптимизации

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,5 @@
+from django.db.models.signals import pre_save, pre_delete
+from django.dispatch import receiver
 from django.shortcuts import get_object_or_404, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.db import transaction
@@ -32,12 +34,13 @@ class OrderItemsCreate(CreateView):
        else:
            basket_items = Basket.get_items(self.request.user)
            if len(basket_items):
-               OrderFormSet = inlineformset_factory(Order, OrderItem, \
+               OrderFormSet = inlineformset_factory(Order, OrderItem,
                                   form=OrderItemForm, extra=len(basket_items))
                formset = OrderFormSet()
                for num, form in enumerate(formset.forms):
                    form.initial['product'] = basket_items[num].product
                    form.initial['quantity'] = basket_items[num].quantity
+                   form.initial['price'] = basket_items[num].product.price
                basket_items.delete()
            else:
                formset = OrderFormSet()
@@ -74,7 +77,11 @@ class OrderItemsUpdate(UpdateView):
        if self.request.POST:
            data['orderitems'] = OrderFormSet(self.request.POST, instance=self.object)
        else:
-           data['orderitems'] = OrderFormSet(instance=self.object)
+           formset = OrderFormSet(instance=self.object)
+           for form in formset.forms:
+               if form.instance.pk:
+                   form.initial['price'] = form.instance.product.price
+           data['orderitems'] = formset
        return data
 
    def form_valid(self, form):
@@ -91,7 +98,7 @@ class OrderItemsUpdate(UpdateView):
        if self.object.get_total_cost() == 0:
            self.object.delete()
 
-       return super(OrderItemsCreate, self).form_valid(form)
+       return super(OrderItemsUpdate, self).form_valid(form)
 
 
 class OrderDelete(DeleteView):
@@ -113,3 +120,20 @@ def order_forming_complete(request, pk):
 
    return HttpResponseRedirect(reverse('orders:orders_list'))
 
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+   if update_fields is 'quantity' or 'product':
+       if instance.pk:
+           instance.product.quantity -= instance.quantity - \
+                                        sender.get_item(instance.pk).quantity
+       else:
+           instance.product.quantity -= instance.quantity
+       instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+   instance.product.quantity += instance.quantity
+   instance.product.save()
